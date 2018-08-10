d038e622eea163930b9938ea235200ef765c6534
MMF <magicmanfoli@gmail.com>
2018-08-10 18:49:46 +0200

    added code to traverse and identify, moving and deleting still needed

--- PFC.py
+++ PFC.py
@@ -1,7 +1,7 @@
 #!python3
 
-from pathlib import Path
 import argparse
+from pathlib import Path
 
 
 def input_path():
@@ -9,7 +9,7 @@ def input_path():
     folder_path = Path(input("Input path to folder:\n>"))
 
     if folder_path == "":
-        print("Goodbye")
+        print("No path given, closing...")
         exit(0)
 
     return folder_path
@@ -23,22 +23,63 @@ def setup_args():
     # read args
     args = parser.parse_args()
 
-    return args.path
+    if args.path == "":
+        return None
+
+    return Path(args.path)
+
+
+# clean pathlib object to contain only folders
+def get_folders(path):
+    folders = []
+
+    for entry in path.iterdir():
+        if entry.is_dir():
+            folders.append(entry)
+
+    return folders
+
 
 # --- main ---
 
 print("Welcome to PortraitFolderCleaner (PFC)\n\n")
+print("This is going to delete all folders in the target direction, be cautious!")
 
 # 1. Get list of folders
 
 path = setup_args()
 
-if path == "":
+if path is None:  # no path in args
     path = input_path()
 
+# TODO remove debug
+path = Path(r"E:\TEST")
 
+print(f"Input path: {path}")
 
-# 2. Delete empty folders from list and drive
+# get list of folders
 
-# 3. iterate over folders and move matching pictures (portraits) to parent directory
+folders = get_folders(path)
 
+confirmation = input(f"folders found: {len(folders)}, continue?[Y]:")
+
+if confirmation != "Y":
+    print("Cancelled, goodbye!")
+    exit(0)
+
+# iterate over folders and move matching pictures (portraits) to parent directory
+
+for folder in folders:
+    for file in folder.glob("*_COVER.jpg"):
+        print(f"moving {file}")
+
+        # move .parent
+
+print("\nEverything moved, deleting remaining folders\n")
+
+# Delete folders from drive
+for folder in folders:
+    print(f"removing {folder}")
+    # folder.rmdir()
+
+print("\nDone.\n")
